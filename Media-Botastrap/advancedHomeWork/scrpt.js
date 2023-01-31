20456ed7ec0666f9ceb4fabb3a6768ea10a58fc2
AndreyNedelevich <nedelevich@gmail.com>
2023-01-31 22:22:56 +0200

    Advance Project 31.01.2023

---
+++ Media-Botastrap/advancedHomeWork/scrpt.js
@@ -0,0 +1,42 @@
+const hamburger = document.querySelector(".hamburger");
+const  menu = document.querySelector(".menu");
+const closeElem = document.querySelector(".menu__close");
+const closeElement = document.querySelector(".menu__overlay");
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
+const btnsSHowModalWindow = document.querySelector(
+    '.btn_show'
+);
+
+hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
+});
+
+closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
+
+});
+
+closeElement.addEventListener("click", () => {
+    menu.classList.remove("active");
+
+});
+
+const openModalWindow = function (e) {
+    e.preventDefault();
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnsSHowModalWindow.addEventListener('click', openModalWindow);
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
+
